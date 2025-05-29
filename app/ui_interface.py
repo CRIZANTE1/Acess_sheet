c481dca04018d804f49a2e6a8d14286998e3e40f
CRISTIAN CARLOS <140450586+CRIZANTE1@users.noreply.github.com>
2025-05-29 12:55:02 -0300

    Update ui_interface.py

--- app/ui_interface.py
+++ app/ui_interface.py
@@ -425,8 +425,8 @@ def vehicle_access_interface():
             else:
                 st.warning(f"Não há registros encontrados para o status {status_filter} e empresa {empresa_filter}.")
          
-    df = st.data_editor(st.session_state.df_acesso_veiculos.fillna(""), num_rows="dynamic")
-    st.session_state.df_acesso_veiculos = df
+    st.table(st.session_state.df_acesso_veiculos.fillna(""))
+    
 
 def blocks():
     sheet_operations = SheetOperations()
