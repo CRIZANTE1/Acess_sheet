a63837c10d185374869f4c256b89c3b6d7f22a09
CRISTIAN CARLOS <140450586+CRIZANTE1@users.noreply.github.com>
2025-06-24 17:29:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import pandas as pd
 from app.ui_interface import vehicle_access_interface
 from app.admin_page import admin_page
 from app.summary_page import summary_page 
@@ -10,32 +9,26 @@ from app.data_operations import load_data_from_sheets
 st.set_page_config(page_title="Controle de Acesso BAERI", layout="wide")
 
 def main():
-    load_data_from_sheets()
+    if 'df_acesso_veiculos' not in st.session_state:
+        load_data_from_sheets()
 
     if is_user_logged_in():
         show_user_header()
         show_logout_button()
-
         user_is_admin = is_admin()
         
-        page_options = [] 
+        page_options = []
         if user_is_admin:
-            page_options.append("Controle de Acesso") 
-            page_options.append("Configurações do Sistema")
-            page_options.append("Resumo")
+            page_options.extend(["Controle de Acesso", "Configurações do Sistema", "Resumo"])
         else:
-            page_options.append("Resumo") 
+            page_options.extend(["Controle de Acesso", "Resumo"])
             
         page = st.sidebar.selectbox("Escolha a página:", page_options)
         
         if page == "Controle de Acesso":
             vehicle_access_interface()
-            
         elif page == "Configurações do Sistema":
-            if user_is_admin:
-                admin_page()
-            else:
-                st.error("Você não tem permissões para acessar esta página.")
+            admin_page()
         elif page == "Resumo": 
             summary_page()
     else:
@@ -45,7 +38,6 @@ def main():
 
 if __name__ == "__main__":
     main()
-
 
 
 
