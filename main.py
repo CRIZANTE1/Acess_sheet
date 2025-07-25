27c8bf3b7b63b90e2aba2ada0f5e54e2f8cf3076
CRISTIAN CARLOS <140450586+CRIZANTE1@users.noreply.github.com>
2025-07-25 11:44:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+
 import streamlit as st
 
+from auth.auth_utils import is_user_logged_in, get_user_role, is_session_expired
+from app.utils import get_sao_paulo_time
+
 from auth.login_page import show_login_page, show_user_header, show_logout_button
-from auth.auth_utils import is_user_logged_in, get_user_role
 from app.data_operations import load_data_from_sheets
 from app.logger import log_action
-
 from app.ui_interface import vehicle_access_interface
 from app.admin_page import admin_page
 from app.summary_page import summary_page 
@@ -16,7 +18,21 @@ def main():
         load_data_from_sheets()
 
     if is_user_logged_in():
+        
+        
+        if 'login_time' not in st.session_state:
+            st.session_state.login_time = get_sao_paulo_time()
 
+        if is_session_expired():
+            st.warning("Sua sessão expirou devido ao horário de troca de turno (07:00 / 19:00). Por favor, faça login novamente.")
+            log_action("SESSION_EXPIRED", "Sessão do usuário expirou automaticamente.")
+            # Limpa chaves da sessão e força o logout
+            if 'login_time' in st.session_state: del st.session_state.login_time
+            if 'login_logged' in st.session_state: del st.session_state.login_logged
+            st.logout()
+            st.rerun()
+
+        
         user_role = get_user_role()
 
         if user_role is None:
@@ -28,6 +44,7 @@ def main():
             log_action("LOGIN", f"Usuário acessou o sistema com papel '{user_role}'.")
             st.session_state.login_logged = True
             
+        
         show_user_header()
         show_logout_button()
         
@@ -37,7 +54,9 @@ def main():
         elif user_role == 'operacional':
             page_options.extend(["Controle de Acesso", "Resumo"])
         
-        page = st.sidebar.selectbox("Escolha a página:", page_options)
+        default_page_index = 0
+        
+        page = st.sidebar.selectbox("Escolha a página:", page_options, index=default_page_index)
         
         if page == "Controle de Acesso":
             vehicle_access_interface()
@@ -46,10 +65,14 @@ def main():
         elif page == "Resumo": 
             summary_page()
     else:
+        if 'login_time' in st.session_state:
+            del st.session_state.login_time
+        if 'login_logged' in st.session_state:
+            del st.session_state.login_logged
+            
         show_login_page()
         
     st.caption('Desenvolvido por Cristian Ferreira Carlos, CE9X,+551131038708, cristiancarlos@vibraenergia.com.br')
 
 if __name__ == "__main__":
     main()
-
